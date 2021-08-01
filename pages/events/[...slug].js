fa0cb4953a79cce5e77d121c8ef68e775c977646
daikiku10 <soccerdk19.910@gmail.com>
2021-08-02 06:24:24 +0900

    Adding Dynamic head Content

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,7 @@
 import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import Head from 'next/head';
 
 import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/EventList';
@@ -63,6 +64,7 @@ const FilteredEventsPage = (props) => {
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <Fragment>
+
         <ErrorAlert>
           <p>選択したイベントが見つかりません！</p>
         </ErrorAlert>
@@ -77,6 +79,11 @@ const FilteredEventsPage = (props) => {
 
   return(
     <Fragment>
+      <Head>
+        <title>Filtered Events</title>
+        <meta name="description" content={`All events for ${numMonth}/${numYear}.`} />
+      </Head>
+
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,11 @@
+import { Fragment } from "react";
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+
 import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/EventList";
 import EventSearch from "../../components/events/EventSearch";
-import { Fragment } from "react";
-import { useRouter } from 'next/router';
 
 const EventsPage = (props) => {
   const { events } = props
@@ -16,6 +19,10 @@ const EventsPage = (props) => {
 
   return (
     <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+      </Head>
       <EventSearch onSearch={findEventsHandler}/>
       <EventList  items={events} />
     </Fragment>
