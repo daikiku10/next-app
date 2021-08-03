2df26dc3807894f28465dfe93d0e141b4d99b1be
daikiku10 <soccerdk19.910@gmail.com>
2021-08-03 14:40:26 +0900

    Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,15 +1,24 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
-    console.log(req.body)
     const userEmail = req.body.email;
-    console.log(userEmail)
 
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({message: '無効なメールアドレスです！'});
       return;
     }
     // 本来はここまで処理が来ればDBへ保存する処理を実装する
-    console.log('12行目', userEmail);
+    const uri = process.env.ATLAS_URI
+    const client = await MongoClient.connect(
+      uri,
+    )
+    const db = client.db();
+
+    await db.collection('emails').insertOne({email: userEmail})
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!'});
   }
 }
